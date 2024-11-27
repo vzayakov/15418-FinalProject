8b6b54bc932a569ed3f2381c020589d8e8c1decd
Victor Zayakov <victor.zayakov@gmail.com>
2024-11-27 18:14:04 -0500

    Finished up writing helper functions for terrain gen

--- main.cpp
+++ main.cpp
@@ -7,26 +7,50 @@
 #include <stdio.h>
 #include <getopt.h>
 #include <string>
+#include <fstream>
+#include <iostream>
 
 #include "terrainGen.h"
 #include "noiseMap.h"
 
-void startTerrainGen(TerrainGen* generator) {
+// Helper function that writes the generated noise map to a .txt file
+void writeNoiseMap(TerrainGen* generator, const int dimX, const int dimY) {
 
   const NoiseMap* noiseMap = generator->getNoiseMap();
+  const std::string outputFilename = "noisemap.txt";
+
+  std::ofstream outNoiseMap(outputFilename, std::fstream::out);
+  if (!outNoiseMap) {
+    std::cerr << "Unable to open file: " << outputFilename << '\n';
+    exit(EXIT_FAILURE);
+  }
+
+  outNoiseMap << dimX << ' ' << dimY << '\n';
+  for (int i = 0; i < dimY; i++) {
+    for (int j = 0; j < dimX; j++) {
+      outNoiseMap << noiseMap->data[i * dimX + j] << ' ';
+    }
+    outNoiseMap << '\n';
+  }
+
+  outNoiseMap.close();
 
 }
 
 int main(int argc, char** argv) {
-
-  int imageSize = 1150;
-
+  // Can modify this to have noise maps of different sizes
+  int noiseMapWidth = 1150;
+  int noiseMapHeight = 1150;
+  // Instantiate the class object
   TerrainGen* generator = new TerrainGen();
 
-  generator->allocOutputNoiseMap(imageSize, imageSize);
+  // Allocate noise map and set up all of the things
+  generator->allocOutputNoiseMap(noiseMapWidth, noiseMapHeight);
   generator->setup();
-
-  startTerrainGen(generator);
+  // Generate the noise map
+  generator->generate();
+  // Write the generated noise map to a .txt file
+  writeNoiseMap(generator, noiseMapWidth, noiseMapHeight);
 
   return 0;
 
